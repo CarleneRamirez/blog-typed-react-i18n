bb61eccfde1c4fd6eb4d5fe7fb25f0256b2a8191
lucky <luckylibora@gmail.com>
2021-10-17 21:41:50 +0300

    added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
+import './i18n'
 import logo from './logo.svg';
 import './App.css';
+import {useTranslation} from "react-i18next";
+import {availableLanguages} from "./i18n";
 
 function App() {
+  const {t, i18n} = useTranslation()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('learnReact')}
         </a>
+        <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
+          {availableLanguages.map((language) => (
+            <option key={language}>{language}</option>
+          ))}
+        </select>
       </header>
     </div>
   );
